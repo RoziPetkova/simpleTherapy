c6aa175742cb1c4fd0b7d3845ef52f1a1f8dca6e
kzdravkov <kzdravkov@vmware.com>
2023-10-25 17:12:19 +0300

    - Smaller images - Add new member - Fix HTML title, description, icon - Delete some unused tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
 //
 // Scripts
 // 
@@ -14,14 +9,14 @@ let sectionPairs = {
     "infoMember4" : "detailsMember4",
     "infoMember5" : "detailsMember5",
     "infoMember6" : "detailsMember6",
+    "infoMember7" : "detailsMember7",
     "paradigm" : "paradigmInfo",
     "process" : "processInfo",
     "session" : "sessionInfo",
     // "conditions" : "issuesInfo",
     // "targerGroup" : "targetgroupInfo",
     // "survices" : "servicesInfo"
-    };
-
+};
 
 window.addEventListener('DOMContentLoaded', event => {
     fetch('assets/papers/statii.json')
@@ -101,8 +96,8 @@ function showDiv() {
 };
 
 $(window).scroll(function() {
-    let kur = `linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgb(36, 30, 30, ${$(window).scrollTop() / 750}) ${120 - $(window).scrollTop() / 7}%)`;
-    $(".containerFade").css("background", kur);
+    let gradient = `linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgb(36, 30, 30, ${$(window).scrollTop() / 750}) ${120 - $(window).scrollTop() / 7}%)`;
+    $(".containerFade").css("background", gradient);
 
     // $(window).scroll(function () { fade(false); }); //fade elements on scroll
 });
@@ -133,9 +128,9 @@ $(window).scroll(function() {
 
 function loadStatii(statii) {
     const lastSixPapers = statii.slice(0, 6);
-    for (let statiq of lastSixPapers) {
-        $("#portfolio .container .row").append(getArticlesContainer(statiq.imgLink, statiq.title, statiq.subtitle, statiq.id));
-        $("#page-top").append(getPopupContainer(statiq.id, statiq.title, statiq.subtitle, statiq.imgLink, statiq.content, statiq.date, statiq.author));
+    for (let article of lastSixPapers) {
+        $("#portfolio .container .row").append(getArticlesContainer(article.imgLink, article.title, article.subtitle, article.id));
+        $("#page-top").append(getPopupContainer(article.id, article.title, article.subtitle, article.imgLink, article.content, article.date, article.author));
     }
 };
 
@@ -187,45 +182,8 @@ function getArticlesContainer(imgLink, statiqTitle, statiqSubtitle, id) {
             </div>`;
 };
 
-// function getArticlesContainer(imgLink, statiqTitle, statiqSubtitle, id) {
-//     return ` <!-- Common Portfolio items will be generated here -->
-//             <div class="col-lg-5 col-sm-2 mb-4"> 
-//                 <div class="portfolio-item"> 
-//                     <a class="portfolio-link" data-bs-toggle="modal" href="#${id}">
-//                         <div class="portfolio-hover"></div> 
-//                         <img class="img-fluid" src="${imgLink}" alt="..."> 
-//                         <p class="bottom-left">Bottom Left</p>
-//                     </a> 
-//                     <div class="portfolio-caption"> 
-//                         <div class="portfolio-caption-heading">${statiqTitle}</div> 
-//                         <div class="portfolio-caption-subheading text-muted">${statiqSubtitle}</div> 
-//                     </div>
-//                 </div>
-//             </div>`;
-// };
-
-// function ShowAdditionalSection(currentSectionId){
-//     var section = document.getElementById(currentSectionId);
-
-//     if (section.style.display == 'none') {
-//         section.style.display = 'block';
-//         console.log("block")
-//     } else {
-//         section.style.display = 'none';
-//         console.log("none")
-//     }
-
-//     scrollIntoViewIfNeeded(section);
-// };
-
 function scrollIntoViewIfNeeded(target) { 
     if (target.getBoundingClientRect().top > window.innerHeight-window.innerHeight*0.2) {
         target.scrollIntoView(top,{block: 'center'});
     }
 };
-
-
-
-
-
-
